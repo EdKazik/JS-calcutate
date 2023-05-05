c11e40482c6647c3bb5f5717aa664989d5d616f4
EdKazik <edoskazik@gmail.com>
2023-05-05 21:29:25 +0200

    upgrade commit

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ const num2 = +prompt("Enter your age:");
 let result;
 
 
-
 if(operation === 'add' || operation === 'sub' || operation === 'mult' || operation === 'div') {
 		result = (`${operation}: ${num1} + ${num2} = ${num1 + num2}`);
-    
 } else {
     alert(`Its not: add, sub, mult, div!!!
-
     Try again!!!!`)
 }
 console.log(result)
@@ -18,9 +15,7 @@ console.log(result)
 if(isNaN(num1) || isNaN(num2)) {
     alert(`Its not a value!!!
   Try again!!!!`)
-
 } else {
 result = (`${num1} + ${num2} = ${num1 + num2}`)
-
 }
 console.log(result)
