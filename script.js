fca8a80db639cef0996564aee1ba7de486042179
EdKazik <edoskazik@gmail.com>
2023-05-06 19:06:34 +0200

    finish commit task1

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ const num2 = +prompt("Enter your age:");
 let result;
 
 if(isNaN(num1) || isNaN(num2)) {
-		alert(`Its not a value!!!!`)
+	alert(`Its not a value!!!!`)
 }
 
 if(operation === 'add') {
-		result = (`${num1} + ${num2} = ${num1 + num2}`);
-} else if(operation === 'sub') {
-		result = (`${num1} - ${num2} = ${num1 - num2}`);
+	result = (`${num1} + ${num2} = ${num1 + num2}`);
+} else if (operation === 'sub') {
+    result = (`${num1} - ${num2} = ${num1 - num2}`);
 } else if(operation === 'mult') {
-  result = (`${num1} * ${num2} = ${num1 * num2}`);
+    result = (`${num1} * ${num2} = ${num1 * num2}`);
 } else if(operation === 'div'){
 	result = (`${num1} / ${num2} = ${num1 / num2}`);
 } else {
-		alert(`I don't know how to act !!!
+	alert(`I don't know how to act !!!
       Try again....`);
 }
